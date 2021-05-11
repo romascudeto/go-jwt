8a781a024b42406f944a5aac895126aad83a4499
Alvin Lesmana <alvin.lesmana@roomme.space>
2021-05-11 16:14:29 +0700

    add auth middleware JWT #3

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
@@ -17,9 +18,22 @@ func Auth(c *gin.Context) {
 
 		return []byte("secret"), nil
 	})
-
+	claims := token.Claims.(jwt.MapClaims)
+	var expiredAt interface{}
+	for key, val := range claims {
+		if key == "expiredAt" {
+			expiredAt = val
+		}
+	}
+	if time.Now().Unix() > int64(expiredAt.(float64)) {
+		result := gin.H{
+			"message": "not authorized",
+			"status":  "token expired",
+		}
+		c.JSON(http.StatusUnauthorized, result)
+		c.Abort()
+	}
 	if token != nil && err == nil {
-		fmt.Println("token verified")
 	} else {
 		result := gin.H{
 			"message": "not authorized",
